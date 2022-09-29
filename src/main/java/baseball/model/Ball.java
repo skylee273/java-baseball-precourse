d886d4725a82a6caa7c2af56d3e92131b99c9d87
skylee273 <skylee273@koreatech.ac.kr>
2022-09-29 23:50:11 +0900

    feat (Ball): Add Ball.validateBallNumber

--- src/main/java/baseball/model/Ball.java
+++ src/main/java/baseball/model/Ball.java
@@ -1,10 +1,24 @@
 package baseball.model;
 
+import baseball.ErrorMessage;
+import baseball.GameMessage;
+
 public class Ball {
+    private static final int MIN_BALL_NUMBER = 1;
+    private static final int MAX_BALL_NUMBER = 9;
     private  int number;
 
     public Ball(int number){
+        validateBallNumber(isBallNumber(number));
         this.number = number;
+    }
+
+    private static boolean isBallNumber(int number){
+        return number >= MIN_BALL_NUMBER &&  number <= MAX_BALL_NUMBER;
+    }
+
+    private void validateBallNumber(boolean isRangeInNumber){
+        if(!isRangeInNumber) throw new IllegalArgumentException(ErrorMessage.ERROR_NOT_NUMBER_IN_RANGE.getErrorMessage());
     }
 
 }
