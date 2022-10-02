ae897ae0332bc698d93b5dce0d19d0c8ab8f4f23
skylee273 <skylee273@koreatech.ac.kr>
2022-10-02 21:39:33 +0900

    feat(BaseBallGame): Add BaseBallGame.start, BaseBallGame.getIsWinning

--- src/main/java/baseball/controller/BaseBallGame.java
+++ src/main/java/baseball/controller/BaseBallGame.java
@@ -10,5 +10,22 @@ public class BaseBallGame {
     public static Balls ready() {
         return new Balls(new RandomNumberGenerator().generateRandomNumberList());
     }
+    public static void start(Balls computer) {
+        Balls user = new Balls(InputView.getManualBalls());
+        BallScore ballScore = user.compareBetweenBalls(computer);
+        if(!getIsWinning(ballScore)) {
+            OutputView.printLossMessage(ballScore.toString());
+            start(computer);
+            return;
+        }
+        OutputView.printWinningMessage(ballScore.toString());
+
+    }
+
+    private static boolean getIsWinning(BallScore ballScore) {
+        return ballScore.getStrikeScore() == 3;
+    }
+
+
 
 }
