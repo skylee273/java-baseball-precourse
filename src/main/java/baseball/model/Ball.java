3b83b07d76f96fda59e0ff59e42d244c919eb735
skylee273 <skylee273@koreatech.ac.kr>
2022-10-02 20:51:33 +0900

    feat (Ball): Add compare, equals, hashCode method

--- src/main/java/baseball/model/Ball.java
+++ src/main/java/baseball/model/Ball.java
@@ -15,6 +15,28 @@ public class Ball {
         this.ballPosition = new BallPosition(ballPosition);
     }
 
+    public BallStatus compare(Ball other) {
+        if (this.equals(other))
+            return BallStatus.STRIKE;
+
+        if (this.ballNumber.equals(other.ballNumber))
+            return BallStatus.BALL;
+
+        return BallStatus.NOTHING;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return Objects.equals(ballPosition, ball.ballPosition) && Objects.equals(ballNumber, ball.ballNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ballPosition, ballNumber);
+    }
 
 
 }
