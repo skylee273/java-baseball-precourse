863c48a77ff304c9a74fee96b5609470d88809b3
skylee273 <skylee273@koreatech.ac.kr>
2022-10-02 21:24:17 +0900

    feat(StringUtil): Add StringUtil.convertStringToInteger

--- src/main/java/baseball/utils/StringUtil.java
+++ src/main/java/baseball/utils/StringUtil.java
@@ -5,6 +5,7 @@ import baseball.ErrorMessage;
 public class StringUtil {
 
     public static boolean isNullOrEmpty(String input) { return (input.isEmpty() || input == null); }
+
     public static void validateNullAndEmpty(boolean isNullOrEmpty){
         if(isNullOrEmpty) throw new IllegalArgumentException(ErrorMessage.ERROR_NULL_AND_EMPTY.getErrorMessage());
     }
