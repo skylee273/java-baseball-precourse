96c4c1c800df1f5ba7974c1a26d4197e66fc4882
skylee273 <skylee273@koreatech.ac.kr>
2022-10-02 09:20:35 +0900

    test (BallTest): ADD Ball Test Case
    BallTest Case
     - BallTest.createTest
     - BallTest.throwExceptionWhenNotThreeBallNumbers ( 야구 번호가 2개일 떄 )
     - BallTest.throwExceptionWhenNotThreeBallNumbers2 ( 야구 번호 4개일 떄 )

--- src/test/java/baseball/model/BallTest.java
+++ src/test/java/baseball/model/BallTest.java
@@ -3,46 +3,50 @@ package baseball.model;
 import baseball.utils.StringUtil;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallTest {
 
-    @DisplayName("볼 생성 테스트")
-    @ParameterizedTest
-    @ValueSource(ints = {123, 123})
-    void createBallTest(int input){
-        assertThat(new Ball(input)).isNotEqualTo(new Ball(input));
-    }
+    private List<BallNumber> ballNumbers = new ArrayList<>();
+    private List<BallNumber> myBallNumbers = new ArrayList<>();
 
-    @DisplayName("숫자가 아닌 값을 받았을때 예외 발생")
-    @ParameterizedTest
-    @ValueSource(strings = {"123", "$", "Ball"})
-    void validateInputTypeTest(int input){
+    @DisplayName("숫자 야구 번호 객체가 3개보다 적으면 예외 발생")
+    @Test
+    void throwExceptionWhenNotThreeBallNumbers() {
+        for(int i = 1; i <=2; i++){
+            ballNumbers.add(new BallNumber(i));
+        }
         assertThatIllegalArgumentException().isThrownBy(() -> {
-           new Ball(input);
+           new Ball(ballNumbers) ;
         });
     }
 
-    @DisplayName("음수 값을 받으면 예외 발생")
-    @ParameterizedTest
-    @ValueSource(ints = {-1})
-    void validateInputNegativeTest(int input){
+    @DisplayName("숫자 야구 번호가 객체가 3개보다 맣으면 예외 발생")
+    @Test
+    void throwExceptionWhenNotThreeBallNumbers2(){
+        for(int i = 1; i <=4; i++){
+            ballNumbers.add(new BallNumber(i));
+        }
         assertThatIllegalArgumentException().isThrownBy(() -> {
-            new Ball(input);
+            new Ball(ballNumbers) ;
         });
     }
 
-    @DisplayName("1~9 이외에 값을 받는지 확인")
-   @ParameterizedTest
-    @ValueSource(ints = {10, 5, 0})
-    void validateRangeNumber(int input){
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            new Ball(input);
-        });
+    @DisplayName("숫자 야구 번호 3개 정상 숫자 야구 객체 발생")
+    @Test
+    void createTest(){
+        for(int i = 1; i <=3; i++){
+            ballNumbers.add(new BallNumber(i));
+        }
+        assertThatCode(() -> new Ball(ballNumbers)).doesNotThrowAnyException();
     }
 
     @DisplayName("숫자가 아닌 값이 입력되면 예외 반환")
