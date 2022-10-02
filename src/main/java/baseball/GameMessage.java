32419d5136fd35c1077df38e972d95250968b455
skylee273 <skylee273@koreatech.ac.kr>
2022-10-02 21:29:43 +0900

    feat(GameMessage): Add GameMessage

---
+++ src/main/java/baseball/GameMessage.java
@@ -0,0 +1,6 @@
+package baseball;
+
+public class GameMessage {
+    public static final String BASEBALL_GAME_START_INPUT = "숫자를 입력해주세요 :";
+    public static final String BASEBALL_GAME_QUIT_MESSAGE = "게임 종료";
+}
